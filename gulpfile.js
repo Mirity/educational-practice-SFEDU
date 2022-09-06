e287e23946868e1f5b70d18c98dc7861b18357f3
Alisa Balackaya <abalatskaya@lachestry.com>
2022-09-06 18:50:01 +0300

    Change include and fix BEM
    [#INDEV22-217]

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ import csso from 'gulp-csso';
 import autoprefixer from 'gulp-autoprefixer';
 import concat from 'gulp-concat';
 import browserSync from 'browser-sync';
-import include from 'gulp-file-include'
 import { deleteSync } from 'del';
 
 const { src, dest, series, parallel, watch } = gulp;
@@ -30,11 +29,8 @@ function scss() {
 
 
 function liquid() {
-    return src('src/templates/*.liquid')
-        .pipe(include({
-            prefix: '@@'
-        }))
-        .pipe(dest('dist/templates'));
+    src('src/templates/*/*.liquid').pipe(dest('dist/templates'));
+    return src('src/templates/*.liquid').pipe(dest('dist/templates'));
 }
 
 function script() {
